7c8395db50e9ccfcb55d7bb86eda59fcf09f1f36
Prasanna K R <developerprash1998@gmail.com>
2022-06-24 18:09:59 +0530

    Time: 84 ms (48.43%), Space: 41.8 MB (84.68%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -4,12 +4,5 @@
  */
 var lengthOfLastWord = function(s) {
     let words = s.trim().split(" ");
-    let lastWord = "";
-    if(words[words.length - 1] == '') {
-        lastWord = words[words.length - 2];
-    } else {
-        lastWord = words[words.length - 1];
-    }
-    console.log(lastWord, lastWord.length);
-    return lastWord.length;
+    return words[words.length - 1].length;
 };
