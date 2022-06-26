32714da157c270cb3b56f5ea5fbf36114549fdf8
Prasanna K R <developerprash1998@gmail.com>
2022-06-26 13:52:15 +0530

    Time: 111 ms (60.54%), Space: 42.2 MB (97.48%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -8,19 +8,20 @@ var isAnagram = function(s, t) {
         return false;
     }
     
-    let freq1 = {};
-    let freq2 = {};
-    for(let val of s) {
-        freq1[val] = (freq1[val] || 0) + 1;
+    let lookup = {};
+    for(let i=0; i<s.length; i++) {  
+        let letter = s[i];
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1;
     }
-    for(let val of t) {
-        freq2[val] = (freq2[val] || 0) + 1;
-    }
-    for(let key in freq1) {
-        if(freq1[key] !== freq2[key]) {
+    
+    for(let i=0; i<t.length; i++) {
+        let letter = t[i];
+        if(!lookup[letter]) {
             return false;
+        } else {
+            lookup[letter] -= 1;
         }
-        // console.log(key);
     }
+    
     return true;
 };
