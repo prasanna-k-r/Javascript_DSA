a0fa860cc35577cd11eeb19a00e5742ff2536248
Prasanna K R <developerprash1998@gmail.com>
2022-06-27 12:02:37 +0530

    Time: 127 ms (5.07%), Space: 42.4 MB (33.81%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -13,10 +13,9 @@ var searchInsert = function(nums, target) {
 function binarySearch(nums, target) {
     let start=0;
     let end=nums.length - 1;
-    const getMedIndex = (s, e) => Math.floor((s + e) / 2);
     
     while(start<=end) {
-        const med = getMedIndex(start, end);
+        const med = Math.floor((start + end) / 2);
         const el = nums[med];
         if (target === el) return med;
         if (target > el) start = med + 1;
