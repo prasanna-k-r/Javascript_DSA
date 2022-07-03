4ee4817709c9577ec4f5e023501e4f9320e034f7
Prasanna K R <developerprash1998@gmail.com>
2022-07-03 12:38:13 +0530

    Time: 123 ms (21.08%), Space: 44.3 MB (51.57%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -4,9 +4,16 @@
  * @return {number[]}
  */
 var shuffle = function(nums, n) {
-    let result = [];
-    for(let i=0; i<n; i++) {
-        result.push(nums[i],nums[n+i]);
+    let output = [];
+    let first = 0;
+    let second = n;
+    
+    for(let i = 0; i < nums.length; i+=2) {
+        output[i] = nums[first];
+        output[i+1] = nums[second];
+        first++;
+        second++;
     }
-    return result;
+    
+    return output;
 };
