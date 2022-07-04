d5c922b32a2b414506378b71605c83f762f6d3f4
Prasanna K R <developerprash1998@gmail.com>
2022-07-04 21:27:33 +0530

    Time: 113 ms (21.65%), Space: 44.2 MB (53.80%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function(sentences) {
+    let maxCount = 0;
+    for(let i=0; i<sentences.length; i++) {
+        let words = sentences[i].split(" ");
+        if(maxCount < words.length) {
+            maxCount = words.length;
+        }
+    }
+    return maxCount;
+};
